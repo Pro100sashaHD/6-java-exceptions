e394b57d55b0190e35651757b06141d6d66a7c32
Pro100sasha_HD <sasha_ablin@rambler.ru>
2024-11-14 17:30:35 +0500

    task 4

---
+++ task04/src/com/example/task04/MyException.java
@@ -0,0 +1,7 @@
+package com.example.task04;
+
+public class MyException extends IllegalArgumentException{
+    public MyException(String exeption_message) {
+        super(exeption_message);
+    }
+}
